9767504c30fb4ee2886e8b3cb5297be5e0ecbecb
Andrew <146958879+Axeborn1@users.noreply.github.com>
2026-01-29 15:15:53 -0600

    Connecting riot and discord apis

--- faker.py
+++ faker.py
@@ -8,14 +8,36 @@ import discord
 import os
 import asyncio
 
+#creates DiscordClient as a child of the client class from discord. This class will manage the discord side of the project.
+class DiscordClient(discord.Client):
+    def __init__(self):
+        #edits the discord client init function to automatically use all intents and cahce flags
+        intents = discord.Intents.all()
+        intents.members = True
+        intents.message_content = True
+        membercacheflags=discord.MemberCacheFlags.from_intents(intents)
 
-class DiscordClient(discord.Client):   
+        #collects the normal discord client init function to make sure the client works correctly
+        super().__init__(intents=intents,member_cache_flags=membercacheflags) 
+
+    #edits the on message event function to make the client automatically respond.
     async def on_message(self, message):
         if message.author == self.user:
             return
 
         if message.content.startswith('Faker!'):
-            await message.channel.send('Hello World!')
+            await message.channel.send(message.content[7:10])
+            print(message.content[7:10])
+            if message.content[7:10]=="add":
+                await message.channel.send("BAZINGA")
+                await self.get_user(message.content[11:])
+    
+    async def get_user(self,riot_id):
+
+        return riot_id
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import api
 import discord
 
 
+#
+#
+#intents = discord.Intents.all()
+#intents.members = True
+#intents.message_content = True
+#membercacheflags=discord.MemberCacheFlags.from_intents(intents)
 
+discord_client = faker.DiscordClient()
+league_trackers = {}
 
-intents = discord.Intents.all()
-intents.members = True
-intents.message_content = True
-membercacheflags=discord.MemberCacheFlags.from_intents(intents)
+@discord_client.event
+async def get_user(riot_id):
+    league_trackers[riot_id] = league.LeagueThing(riot_id)
+    print(league_trackers)
 
-discord_client = faker.DiscordClient(intents=intents,member_cache_flags=membercacheflags)
 
 discord_client.run(api.discord_token)
